c50067784ee1a183e038ce031a77bc590b7e7faa
Maruf <hossain.maruf186@gmail.com>
2026-01-08 23:35:36 -0500

    configured router

--- rss-aggregator/go.mod
+++ rss-aggregator/go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/go-chi/chi v1.5.5
 	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/rs/cors v1.11.1
--- rss-aggregator/main.go
+++ rss-aggregator/main.go
@@ -24,14 +24,20 @@ func main() {
 
 	router := chi.NewRouter()
 
-	router.Use(cors.Handler(cors.Options{
+	router.Use(cors.New(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"*"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 		MaxAge:           300,
-	}))
+	}).Handler)
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
